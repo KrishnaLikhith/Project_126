8c410dd1608210619ee8c31aaa401400cc74cf61
KrishnaLikhith <76393325+KrishnaLikhith@users.noreply.github.com>
2021-10-27 17:48:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,52 +11,24 @@ scorer = 0;
 
 
 function preload() {
-
+    believer = loadSound("Believer.mp3");
+    phoenix = loadSound("Phoenix.mp3");
 
 }
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
-    believer = loadSound("Believer.mp3");
-    phoenix = loadSound("Phoenix.mp3");
     video = createCapture(VIDEO);
     video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPosses);
 
-}
-function draw() {
-    image(video, 0, 0, 600, 500);
-    songPlayingb = believer.isPlaying();
-    songPlayingp = phoenix.isPlaying();
-    console.log("Song Status : Believer");
-    if (scorel > 0.2) {
-        fill("#FF0000");
-        stroke("#FF0000");
-        circle(leftwX, leftwY, 10);
-        phoenix.stop();
-        if (songPlayingp == false) {
-            believer.play();
-            document.getElementById("song").innerHTML = "Song Playing - Believer";
-        }
+} 
 
-    }
-    if (scorer > 0.2) {
-        fill("#FF0000");
-        stroke("#FF0000");
-        circle(rightwX, rightwY, 10);
-        believer.stop();
-        if ( songPlayingb == false) {
-            phoenix.play();
-            document.getElementById("song").innerHTML = "Song Playing - Phoenix";
-        }
-    }
-
-
-}
 function modelLoaded() {
     console.log("Model is Loaded");
 }
+
 function gotPosses(results) {
     if (results.length > 0) {
         console.log(results);
@@ -65,13 +37,41 @@ function gotPosses(results) {
         console.log("Left Hand Wrist is at " + leftwX + " " + leftwY);
         rightwX = results[0].pose.rightWrist.x;
         rightwY = results[0].pose.rightWrist.y;
-        console.log("Right Hand Wrist is at" + rightwX + " " + rightwY);
+        console.log("Right Hand Wrist is at " + rightwX + " " + rightwY);
         scorel = results[0].pose.keypoints[9].score;
         scorer = results[0].pose.keypoints[10].score;
+
     }
 }
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+    songPlayingb = believer.isPlaying();
+    songPlayingp = phoenix.isPlaying();
+    if (scorel > 0.2) {
+        circle(leftwX, leftwY, 10);
+        phoenix.stop();
+        if (songPlayingb == false) {
+            believer.play();
+            document.getElementById("song").innerHTML = "Song Playing - Believer";
+        }
+
+    }
+    if (scorer > 0.2) {
+        circle(rightwX, rightwY, 10);
+        believer.stop();
+        if (songPlayingp == false) {
+            phoenix.play();
+            document.getElementById("song").innerHTML = "Song Playing - Phoenix";
+        }
+    }
+
+
+}
+
+
 function play() {
-    believer.play();
-    believer.setVolume(1);
-    believer.rate(1);
+    phoenix.play();
+    phoenix.setVolume(1);
+    phoenix.rate(1);
 }
